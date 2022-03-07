cf845b0fd7d412c7f93a83798cc09d51f11dd413
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-03-07 08:52:42 +0000

    Added : Feature to insert employee with thread

---
+++ EmployeePayrollOpreation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingMultiThreading
+{
+    public class EmployeePayrollOpreation
+    {
+        public List<EmployeeDetails> employeePayrollDetailList = new List<EmployeeDetails>();
+
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee Being Added : " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added successfully" + employeeData.EmployeeId);
+            });
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+
+        public void addEmployeeToPayrollWithThread(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData => 
+            {
+                Task thread = new Task(() =>
+               { 
+                   Console.WriteLine("Employee being added : ",employeeData.EmployeeName);
+                   this.addEmployeePayroll(employeeData);
+                   Console.WriteLine("Employee Added successfully : ", employeeData.EmployeeName);
+
+               });
+            });
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+
+        private void addEmployeePayroll(EmployeeDetails employeeData)
+        {
+            employeePayrollDetailList.Add(employeeData);
+        }
+    }
+}
