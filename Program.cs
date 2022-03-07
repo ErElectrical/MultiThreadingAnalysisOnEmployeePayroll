77845830770458af005b7a6942d0e66c5e4b2d33
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-03-07 05:53:15 +0000

    Added : Main class of Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeePayrollUsingMultiThreading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** welcome to Employee Payroll using MultiThreading ");
+
+        }
+    }
+}
+
+
